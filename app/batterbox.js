3196bbddcf17609d2623d6e7171737bbfdf22426
Chris White <cxwembedded@gmail.com>
2022-11-24 15:06:46 -0500

    WIP positioning batterboxes

--- app/batterbox.js
+++ app/batterbox.js
@@ -16,6 +16,9 @@ let Two = require('two.js');
  * @param {int} h Height
  */
 class BatterBox {
+    // User-specified bounding-box coordinates
+    bbox = {};
+
     // two.js coordinates
     twoX;
     twoY;
@@ -32,8 +35,10 @@ class BatterBox {
     onStrikeIcon;
 
     constructor(ulx, uly, w, h, styles = {}, onStrike = false) {
-        const VSHIFT = 5; // XXX TODO figure out a better way to center
-        // alignment=baseline text
+        this.bbox.ulx = ulx;
+        this.bbox.uly = uly;
+        this.bbox.w = w;
+        this.bbox.h = h;
 
         // Grid params: where to put the components as a percentage of width
         // TODO make parameterizable
@@ -49,39 +54,36 @@ class BatterBox {
         this.twoX = ulx + w / 2;
         this.twoY = uly + h / 2;
 
-        this.outline = new Two.Rectangle(this.twoX, this.twoY, w, h);
-        this.outline.fill = 'none';
+        // Create shapes.  All have baseline x = 0, y=0.  addTo() will
+        // position the group containing the shapes.
 
         // Name: left-aligned
         styles.alignment = 'left';
-        this.tName = new Two.Text('name', ulx + leftPadding, this.twoY +
-            VSHIFT,
-            styles);
+        this.tName = new Two.Text('name', leftPadding, 0, styles);
 
         // Runs and balls
         styles.alignment = 'right';
-        this.tRuns = new Two.Text('42', ulx + w * (namePct + runsPct),
-            this.twoY + VSHIFT, styles);
+        this.tRuns = new Two.Text('42', w * (namePct + runsPct), 0, styles);
 
         styles.size = 'small'; // TODO make parameterizable
-        this.tBalls = new Two.Text('84', ulx + w * (namePct + runsPct +
-            ballsPct), this.twoY + VSHIFT, styles);
+        this.tBalls = new Two.Text('84', w * (namePct + runsPct + ballsPct),
+            0, styles);
 
         // Labels
         styles.alignment = 'left';
 
-        this.runsLabel = new Two.Text('R', ulx + w * (namePct + runsPct),
-            this.twoY + VSHIFT, styles);
-        this.ballsLabel = new Two.Text('B', ulx + w * (namePct + runsPct +
-            ballsPct), this.twoY + VSHIFT, styles);
+        this.runsLabel = new Two.Text('R', w * (namePct + runsPct), 0,
+            styles);
+        this.ballsLabel = new Two.Text('B',
+            w * (namePct + runsPct + ballsPct), 0, styles);
 
-        this.group = new Two.Group(this.outline, this.tName, this.tRuns,
+        this.group = new Two.Group(this.tName, this.tRuns,
             this.runsLabel, this.tBalls, this.ballsLabel);
 
         // On strike?
-        if (onStrike) {
-            this.onStrikeIcon = new Two.ImageSequence(['bat-icon.png'], ulx,
-                this.twoY, 0);
+        if (false /* XXX */ && onStrike) {    // TODO put this back
+            this.onStrikeIcon = new Two.ImageSequence(['bat-icon.png'], 0,
+                0, 0);
             this.onStrikeIcon.scale = 0.15; // hack --- FIXME
             this.group.add(this.onStrikeIcon);
         }
@@ -89,7 +91,30 @@ class BatterBox {
     }
 
     addTo(two) {
-        this.group.addTo(two)
+        // Add the group off-screen so we can find out where it is.
+        this.group.position.x = this.group.position.y = -1000;
+        this.group.addTo(two);
+        const where = this.group.getBoundingClientRect();
+
+        // Move the group horizontally, left-justified
+        // E.g., where.left = -1010 => move position.x +10 (right)
+        this.group.position.x = this.bbox.ulx + (-1000 - where.left);
+
+        // Move the group vertically, with center vspacing.
+        // First, get the baseline location.  E.g., top = -1010 means
+        // the top is 10px above the baseline === baseline is 10px below
+        // the top.
+        const dyTopToBaseline = -1000 - where.top;
+        const vmargin = this.bbox.h - where.height;
+        this.group.position.y = this.bbox.uly + (vmargin/2) + dyTopToBaseline;
+
+        // Add the outline now that we know where
+        this.outline = new Two.Rectangle(this.twoX, this.twoY, this.bbox.w,
+            this.bbox.h);
+        this.outline.fill = 'none';
+        this.group.add(this.outline);
+
+
     }
 
     set name(value) {
