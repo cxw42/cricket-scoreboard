0ba4130224a2ac984745dd198179b8e7f1655b66
Chris White <cxwembedded@gmail.com>
2022-12-09 21:26:13 -0500

    Hack: show a score for the bowling team

--- app/inningsbox.js
+++ app/inningsbox.js
@@ -125,6 +125,16 @@ class InningsBox {
             ]);
         this.tTeam1Score.addTo(this.scoresGroup);
 
+        styles.fill = '#000';   // XXX
+        this.tTeam2Score = new Textbox(svg, w*0.5+45    // XXX
+            , 0, w*0.3, h, 'tl', [{text: '123', styles: Utils.extend(styles, {
+                        'class': 'inningsFigures',
+                        'font-size': scoreTextSize,
+                    })
+                }]);
+        this.tTeam2Score.addTo(this.scoresGroup);
+
+
         this.group.add(this.scoresGroup);
 
         // Bowling side
